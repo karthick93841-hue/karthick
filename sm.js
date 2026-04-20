6df9703231b79b2df4841c80b7a18d39e945c1ae
karthick93841-hue <karthick93841@gmail.com>
2026-04-20 15:08:56 +0530

    scv

---
+++ sm.js
@@ -0,0 +1,32 @@
+// show menu 
+
+const showmenu =(toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click' , () =>{
+             nav.classList.toggle('show')
+
+        });
+
+    }
+}
+
+showmenu('nav_toggle', 'nav_menu')
+
+// active removie active
+
+const navlink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+
+    navlink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navmenu = document.getElementById('nave_menu')
+    navmenu.classList.remove('show')
+
+}
+
+ navlink.forEach(n => n.addEventListener('click', linkAction))
